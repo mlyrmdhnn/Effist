d06f90d89b4fdcee9cb29534b11b3c0e93bb0d6f
mlyrmdhnn <mulyaranadhan@gmail.com>
2025-11-20 22:39:25 +0700

    idle logout feature

--- src/App.vue
+++ src/App.vue
@@ -9,4 +9,41 @@
 <script setup lang="ts">
 import ThemeProvider from './components/layout/ThemeProvider.vue'
 import SidebarProvider from './components/layout/SidebarProvider.vue'
+import { onMounted, onUnmounted } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+
+let timeout: any = null
+
+const idle_time = 10 * 60 * 15000
+
+const autoLogout = () => {
+  localStorage.removeItem('token')
+  router.push('/')
+}
+
+const resetTimer = () => {
+  clearTimeout(timeout)
+  timeout = setTimeout(autoLogout, idle_time)
+}
+
+onMounted(() => {
+
+  window.addEventListener('mousemove', resetTimer)
+  window.addEventListener('keydown', resetTimer)
+  window.addEventListener('click', resetTimer)
+  window.addEventListener('scroll', resetTimer)
+
+  resetTimer()
+})
+
+onUnmounted(() => {
+  window.removeEventListener('mousemove', resetTimer)
+  window.removeEventListener('keydown', resetTimer)
+  window.removeEventListener('click', resetTimer)
+  window.removeEventListener('scroll', resetTimer)
+  clearTimeout(timeout)
+})
+
 </script>
